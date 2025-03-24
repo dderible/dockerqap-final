9ebc57b5a07bcc6185530e3804db261597828a75
dderible <declan.derible@keyin.com>
2025-03-24 09:08:14 -0230

    finished work from last night

--- src/main/java/com/example/golfclubqap/tournaments/TournamentRepository.java
+++ src/main/java/com/example/golfclubqap/tournaments/TournamentRepository.java
@@ -1,1 +1,13 @@
 package com.example.golfclubqap.tournaments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+    List<Tournament> findByStartDate(LocalDate startDate);
+    List<Tournament> findByLocation(String location);
+}
